a5a9d277844bb3a41972e89afbd00b36c877f31e
Anya L Keller <kelleranyal@gmail.com>
2019-11-19 13:32:00 -0500

    got merge sort to display solved

--- public/js/sorts/mergesort.js
+++ public/js/sorts/mergesort.js
@@ -30,7 +30,9 @@ class MergeSort extends SortClass {
       .catch(poop => console.log(poop));
   }
 
-  merge(ans, dataAns, f, l, part1, part2) {
+  mergeWhileLoop;
+
+  merge(ans, dataAns, f, l, part1, part2, wherewasi) {
     let first = part1.ans;
     let last = part2.ans;
     let firstData = part1.dataAns;
@@ -39,54 +41,80 @@ class MergeSort extends SortClass {
       if (first[f] < last[l]) {
         ans.push(first[f]);
         dataAns.push(firstData[f]);
-
+        this.dataBarUtils.resizeBars(
+          this.data[wherewasi],
+          ans[ans.length - 1],
+          this.maxNum
+        );
         f++;
       } else {
         ans.push(last[l]);
         dataAns.push(lastData[l]);
+        this.dataBarUtils.resizeBars(
+          this.data[wherewasi],
+          ans[ans.length - 1],
+          this.maxNum
+        );
         l++;
       }
+			wherewasi++;
     }
 
-    if (f == first.length) {
-      console.log(ans);
+    if (l < last.length) {
+      console.log(this.data);
+      for (var position = l; position < last.length; position++) {
+        console.log(wherewasi + position + ans.length);
+        ans.push(last[position]);
+        this.dataBarUtils.resizeBars(
+          this.data[wherewasi],
+          ans[ans.length - 1],
+          this.maxNum
+        );
+				wherewasi++;
+      }
       return {
-        ans: ans.concat(last.slice(l)),
+        ans: ans,
         dataAns: dataAns.concat(lastData.slice(l))
       };
-    } else if ((l = last.length)) {
-      console.log(ans);
+    } else if (f < first.length) {
+      for (var position = f; position < first.length; position++) {
+        console.log(wherewasi + position + ans.length);
+        ans.push(first[position]);
+        this.dataBarUtils.resizeBars(
+          this.data[wherewasi],
+          ans[ans.length - 1],
+          this.maxNum
+        );
+				wherewasi++;
+      }
       return {
-        ans: ans.concat(first.slice(f)),
+        ans: ans,
         dataAns: dataAns.concat(firstData.slice(f))
       };
     } else {
+      console.log('else');
       return {ans: ans, dataAns: dataAns};
     }
   }
 
-  mergeSort(arr, dataSection) {
+  mergeSort(arr, dataSection, wherewasi) {
     if (arr.length > 1) {
       var end = arr.length - 1;
       var middle = Math.floor((end + 1) / 2);
       var part1 = this.mergeSort(
         arr.slice(0, middle),
-        dataSection.slice(0, middle)
+        dataSection.slice(0, middle),
+        wherewasi
       );
       var part2 = this.mergeSort(
         arr.slice(middle, end + 1),
-        dataSection.slice(middle, end + 1)
+        dataSection.slice(middle, end + 1),
+        wherewasi + middle
       );
-      if (typeof part1 != 'undefined' && typeof part2 != 'undefined') {
-        var mergeResult = this.merge([], [], 0, 0, part1, part2);
-        return mergeResult;
-      } else {
-        console.log('both undefined ugh');
-        return {ans: arr, dataAns: dataSection};
-      }
+      var mergeResult = this.merge([], [], 0, 0, part1, part2, wherewasi);
+      return mergeResult;
     } else {
-      console.log('ahhh');
-      console.log(arr);
+      this.dataBarUtils.resizeBars(this.data[wherewasi], arr[0], this.maxNum);
       return {ans: arr, dataAns: dataSection};
     }
   }
@@ -96,7 +124,7 @@ class MergeSort extends SortClass {
     return new Promise((res, rej) => {
       clearTimeout(this.currentTimeOut);
       this.currentTimeOut = null;
-      console.log(this.mergeSort(this.currentar, this.data));
+      console.log(this.mergeSort(this.currentar, this.data, 0));
       res(true);
     });
   }
