15f2da22126a755436b43bbda8ebb4a21429d7da
Anya L Keller <kelleranyal@gmail.com>
2019-11-19 21:15:13 -0500

    officialy began transfer to react

--- sorts_and_searches/src/App.js
+++ sorts_and_searches/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import SortOptns from './components/sortOptns';
-import QuickSortOptns from './components/quickSortOptns';
 import Controls from './components/controls.js';
 import AlgoStatus from './components/algoStatus.js';
 import Visualization from './components/visualization.js';
@@ -9,9 +8,8 @@ function App() {
   return (
     <div>
       <SortOptns />
-      <QuickSortOptns />
-      <Controls />
       <AlgoStatus />
+      <Controls />
       <Visualization />
     </div>
   );
